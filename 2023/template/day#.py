aea5e7c576c1458692cdce515d9a2e63326edf75
Ian <ianlum314@gmail.com>
2023-12-02 00:34:27 -0500

    make template day

---
+++ 2023/template/day#.py
@@ -0,0 +1,18 @@
+file_idx = 0
+# real = 0, test = 1
+
+files = ["day#_input.txt", "day#_test.txt"]
+with open(files[file_idx]) as f:
+    lines = f.read().splitlines()
+
+
+def part1(lines):
+    pass
+
+
+def part2(lines):
+    pass
+
+
+print("Part 1:", part1(lines))
+print("Part 2:", part2(lines))
