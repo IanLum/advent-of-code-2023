6e42308a227bd6d319b9dfb2d222e4c5f4c4592a
Ian Lum <ianlum314@gmail.com>
2023-12-13 23:49:55 -0500

    day 12 part 2 woooooo

--- 2023/day12/day12.py
+++ 2023/day12/day12.py
@@ -1,10 +1,11 @@
-file_idx = 1
+file_idx = 0
 # real = 0, test = 1
 
 from itertools import product
+from functools import cache
 import re
 
-files = ["day12_input.txt", "day12_test.txt"]
+files = ["day12_input.txt", "day12_test.txt", "test2.txt"]
 with open(files[file_idx]) as f:
     lines = f.read().splitlines()
 
@@ -16,8 +17,6 @@ def part1(lines):
         mapp = list(map(int, mapp.split(",")))
         unknown = springs.count("?")
         combs = ["".join(x) for x in product(["#", "."], repeat=unknown)]
-        # if i % 20 == 0:
-        #     print(i)
         for comb in combs:
             ct = 0
             attempt = ""
@@ -33,12 +32,74 @@ def part1(lines):
     return summ
 
 
+@cache
+def arrangements(springs: str, damaged: tuple, debug=False):
+    springs = springs.lstrip(".")
+    if debug:
+        print(springs, damaged)
+
+    # if no more groups, it's a valid arrangement if there are no more damaged springs
+    if damaged == ():
+        if "#" not in springs:
+            if debug:
+                print("success")
+            return 1
+        else:
+            if debug:
+                print("damaged empty")
+            return 0
+
+    # at this point, we know damaged isn't empty, so we expect to have damaged or unknown springs in the springs string
+    if "#" not in springs and "?" not in springs:
+        if debug:
+            print("expected damaged springs, found none")
+        return 0
+
+    # now we know there are damaged springs, and damaged spring groups we're looking for
+
+    # if we see a ? in front, try either option ., or #
+    if springs[0] == "?":
+        if debug:
+            print("branching")
+        return arrangements("." + springs[1:], damaged, debug=debug) + arrangements(
+            "#" + springs[1:], damaged, debug=debug
+        )
+    # at this point, we know it's a # in front
+    else:
+        fst_group = damaged[0]
+
+        # can't make first group
+        # either there aren't enough springs or a working spring (.) is in the way
+        if len(springs) < fst_group or "." in springs[:fst_group]:
+            if debug:
+                print("can't make first group")
+            return 0
+
+        # from the if above, we already know there are only ?s and #s in the length of the first group
+        # check to make sure the group ends with a . or ?
+        elif springs[fst_group] == "#":
+            # if theres a # after the first group, fail
+            if debug:
+                print("fails because # after first group")
+            return 0
+
+        # at this point, the first group is valid
+        else:
+            # keep solving after the group
+            if debug:
+                print("moving on")
+            return arrangements(springs[fst_group + 1 :], damaged[1:], debug=debug)
+
+
 def part2(lines):
     summ = 0
     for i, line in enumerate(lines):
         [springs, mapp] = line.split(" ")
         springs = springs + ("?" + springs) * 4
-        mapp = list(map(int, mapp.split(","))) * 5
+        mapp = tuple(map(int, mapp.split(","))) * 5
+        a = arrangements(springs + ".", mapp, debug=False)
+        summ += a
+    return summ
 
 
 # print("Part 1:", part1(lines))
