de33f8dddf36c3b08dce5d6151b09ff54dbd4c69
Ian Lum <ianlum314@gmail.com>
2024-03-12 19:39:26 -0400

    refactor find area, write part 2

--- kotlin_2023/src/main/kotlin/day18.kt
+++ kotlin_2023/src/main/kotlin/day18.kt
@@ -1,7 +1,7 @@
 import java.io.File
 import kotlin.math.abs
 
-const val fileIdx = 0
+const val fileIdx = 1
 // real = 0, test = 1
 val files = listOf("day18", "day18_test")
 
@@ -9,7 +9,8 @@ data class Vector(val x: Int, val y: Int) {
     operator fun plus(v: Vector) = Vector(x + v.x, y + v.y)
     operator fun times(i: Int) = Vector(i*x, i*y)
 }
-fun part1(lines: List<String>): Int {
+
+fun findArea(instructions: List<Pair<String, Int>>): Int {
     var curr = Vector(0,0)
     val points = mutableListOf(Vector(0,0))
 
@@ -21,11 +22,10 @@ fun part1(lines: List<String>): Int {
     )
 
     var perim = 0
-    lines.forEach {
-        val (dir, distStr) = it.split(" ")
-        val dist = distStr.toInt()
-        curr += dirMap[dir]!! * dist
-        perim += dist
+    instructions.forEach {
+        println("${it.first}, ${it.second}")
+        curr += dirMap[it.first]!! * it.second
+        perim += it.second
         points.add(curr)
     }
     var sum = 0
@@ -34,8 +34,30 @@ fun part1(lines: List<String>): Int {
     }
     return abs(sum/2) + perim/2 + 1
 }
+fun part1(lines: List<String>): Int {
+    val instructions = mutableListOf<Pair<String, Int>>()
+    lines.forEach {
+        val (dir, dist) = it.split(" ")
+        instructions.add(Pair(dir, dist.toInt()))
+    }
+    return findArea(instructions)
+}
+
+fun part2(lines: List<String>): Int {
+    val instructions = mutableListOf<Pair<String, Int>>()
+    val numToDir = mapOf('0' to "R", '1' to "D", '2' to "L", '3' to "U")
+    lines.forEach {
+        val hex = it.split("#")[1]
+        instructions.add(Pair(
+            numToDir[hex[5]]!!,
+            hex.take(5).toInt(radix = 16)
+        ))
+    }
+    return findArea(instructions)
+}
 
 fun main() {
     val lines = File("data/${files[fileIdx]}.txt").readLines()
-    println(part1(lines))
+//    println(part1(lines))
+    println(part2(lines))
 }
